f9611942e987430cbd824c88d540da3a7574941e
Danh Nguyen <ncdanhvn@gmail.com>
2023-11-16 14:08:07 +0700

    final

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
   <div class="wrapper">
-    <div class="drop-box">
+    <div
+      class="drop-box"
+      @drop.prevent="onDrop($event)"
+      @dragenter.prevent
+      @dragover.prevent
+    >
       <ul>
         Drop items here
         <li v-for="item in dropList">{{ item }}</li>
       </ul>
     </div>
+    <p>Count is: {{ count }}</p>
     <div
       v-for="label in labelList"
       class="label"
       :style="{ top: `${label.x}px`, left: `${label.y}px` }"
+      draggable="true"
+      @dragstart="handleDragStart($event, label)"
     >
       {{ label.name }}
     </div>
@@ -23,7 +31,18 @@ import { Label, generateLabels } from "./utils";
 const dropList = ref<string[]>([]);
 const labelList = ref<Label[]>([]);
 
+const count = ref(0);
+
 labelList.value = generateLabels();
+
+const handleDragStart = (event: DragEvent, label: Label) => {
+  event.dataTransfer?.setData("text/plain", label.name);
+};
+
+const onDrop = (event: DragEvent) => {
+  count.value++;
+  console.log(event.dataTransfer?.getData("text/plain"));
+};
 </script>
 
 <style scoped>
